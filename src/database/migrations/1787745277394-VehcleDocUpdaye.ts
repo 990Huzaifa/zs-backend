59d685cf09c9c1cd535e238e075d99706a6674f0
syed-huzaifa-ali <syedhuzaifaali366@gmail.com>
2026-08-26 16:55:14 +0500

    Remove obsolete VehcleDocUpdaye migration file

--- src/database/migrations/1787745277394-VehcleDocUpdaye.ts
+++ src/database/migrations/1787745277394-VehcleDocUpdaye.ts
@@ -1,16 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class VehcleDocUpdaye1787744915233 implements MigrationInterface {
-    name = 'VehcleDocUpdaye1787744915233'
+export class VehcleDocUpdaye1787745277394 implements MigrationInterface {
+    name = 'VehcleDocUpdaye1787745277394'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "vehicle_documents" DROP CONSTRAINT "FK_vehicle_documents_vehicleId"`);
+        await queryRunner.query(`ALTER TABLE "vehicle_documents" ADD "vehicleId" uuid NOT NULL`);
         await queryRunner.query(`ALTER TABLE "vehicle_documents" ADD CONSTRAINT "FK_61c94219ae61c3752cf1e6582c7" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "vehicle_documents" DROP CONSTRAINT "FK_61c94219ae61c3752cf1e6582c7"`);
-        await queryRunner.query(`ALTER TABLE "vehicle_documents" ADD CONSTRAINT "FK_vehicle_documents_vehicleId" FOREIGN KEY ("vehicleId") REFERENCES "vehicles"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "vehicle_documents" DROP COLUMN "vehicleId"`);
     }
 
 }
